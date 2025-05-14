e8bca2287e3e3c34f901ac2a50f7b9918c474941
Fady Elshahat <fady.elshahat@gmail.com>
2025-05-14 14:49:52 +0300

    creational design patterns => Fectory Pattern

--- creational pattern/factory_method_pattern.js
+++ creational pattern/factory_method_pattern.js
@@ -1,32 +1,45 @@
-function Developer( name ) {
-     this.name = name;
-     this.type = "Developer";
+// Base class for all employees
+class Employee {
+     constructor ( name, type ) {
+          this.name = name;
+          this.type = type;
+     }
+
+     sayHi() {
+          console.log( `Hi, I am ${ this.name } and I am a ${ this.type }` );
+     }
 }
 
-function Tester( name ) {
-     this.name = name;
-     this.type = "Tester";
+// Developer class extending Employee
+class Developer extends Employee {
+     constructor ( name ) {
+          super( name, "Developer" );
+     }
 }
 
-function EmployeeFactory() {
-     this.create = ( name, type ) => {
+// Tester class extending Employee
+class Tester extends Employee {
+     constructor ( name ) {
+          super( name, "Tester" );
+     }
+ }
+
+
+// Factory class to create employees based on type
+class EmployeeFactory {
+     create( name, type ) {
           switch ( type ) {
                case 1:
-                    return new Developer( name )
-                    break;
+                    return new Developer( name );
                case 2:
-                    return new Tester( name )
-                    break;
+                    return new Tester( name );
                default:
-                    break;
+                    throw new Error( "Invalid employee type" );
           }
      }
 }
 
-function sayHi() {
-     console.log( "Hi, I am " + this.name + "and I am a " + this.type )
-}
-
+// Using the factory
 const employeeFactory = new EmployeeFactory();
 const employees = [];
 
@@ -34,6 +47,5 @@ employees.push( employeeFactory.create( 'Fady Elshahat', 1 ) );
 employees.push( employeeFactory.create( 'Amr Saad', 1 ) );
 employees.push( employeeFactory.create( 'Remon Ramzy', 2 ) );
 
-employees.map( ( emp ) => {
-     sayHi.call( emp )
-} )
+// Each employee says hi
+employees.forEach( emp => emp.sayHi() );
